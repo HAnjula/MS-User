7c81878d6160748bd3d90bd66520dfe9891b3dee
anjula hadapangoda <handapangodageachinthana@gmail.com>
2025-06-01 00:19:46 +0530

    Initial commit with Spring Boot microservices

---
+++ src/main/java/com/microservices/user/api/UserController.java
@@ -0,0 +1,15 @@
+package com.microservices.user.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getAllUsers(){
+        return "List of Users";
+    }
+}
